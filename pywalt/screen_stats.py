296999f16f8085d735c2fe47bc1fdadc5b17a25d
Mark Koudritsky <kamrik@google.com>
2016-10-26 13:21:43 -0400

    Add vsync timestamp to screen test

--- pywalt/screen_stats.py
+++ pywalt/screen_stats.py
@@ -8,28 +8,34 @@ def screen_stats(blinker_file_name, sensor_file_name):
 
     # Convert all times to milliseconds
     t_sensor = sensor_data[:, 0] * 1000
-    t_blinker = blinker_data / 1e3
+    t_enqueue = blinker_data[:, 0] / 1e3
+    t_vsync = blinker_data[:, 1] / 1e3
 
     # Throw away any sensor timestamps earlier than the first blink
     # this may happen if the operator attached the sensor after
     # running the command. But this should be avoided.
-    skip_sensor = sum(t_sensor < t_blinker[0])
+    skip_sensor = sum(t_sensor < t_enqueue[0])
     if(skip_sensor):
         t_sensor = t_sensor[skip_sensor:]
         print "Skipped first %d readings from the sensor" % skip_sensor
 
     # Get only the common size
-    length = min(len(t_sensor), len(t_blinker))
+    length = min(len(t_sensor), len(t_enqueue))
     t_sensor = t_sensor[1:length]
-    t_blinker = t_blinker[1:length]
+    t_enqueue = t_enqueue[1:length]
+    t_vsync = t_vsync[1:length]
 
     # Shift time so that first time point is 0
-    t0 = min(t_blinker)
+    t0 = min(t_enqueue)
     t_sensor = t_sensor - t0
-    t_blinker = t_blinker - t0
+    t_enqueue = t_enqueue - t0
+    t_vsync = t_vsync - t0
 
-    dt = t_sensor - t_blinker
-    print('t = array([' + ', '.join(map(str, dt)) + '])')
+    dt = t_sensor - t_vsync
+    dte = t_vsync - t_enqueue
+    print('dte = array([' + ', '.join(map(str, dte)) + '])')
+    print('dt = array([' + ', '.join(map(str, dt)) + '])')
+
 
     print("Screen response [ms] median: %0.1f, stdev: %0.2f" %
             (numpy.median(dt), numpy.std(dt)))
